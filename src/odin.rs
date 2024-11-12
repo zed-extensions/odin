f0d6f0000d82d5f71ef2cc088d2825cd47d962c9
Mo Nematzadeh <mohammad.h.nematzadeh@gmail.com>
2024-11-12 22:27:04 +0100

    First working code

---
+++ src/odin.rs
@@ -0,0 +1,145 @@
+use std::fs;
+use zed::{LanguageServerId, Worktree};
+use zed_extension_api::{self as zed, serde_json, settings::LspSettings, Result};
+
+struct OdinExtension {
+    cached_binary_path: Option<String>,
+}
+
+const GITHUB_REPO: &str = "DanielGavin/ols";
+
+impl OdinExtension {
+    fn language_server_binary_path(
+        &mut self,
+        language_server_id: &LanguageServerId,
+        worktree: &Worktree,
+    ) -> Result<String> {
+        let language_server = language_server_id.as_ref();
+        if let Some(path) = LspSettings::for_worktree(language_server, worktree)
+            .ok()
+            .and_then(|settings| settings.binary)
+            .and_then(|binary| binary.path)
+        {
+            return Ok(path);
+        }
+
+        if let Some(path) = worktree.which(language_server) {
+            return Ok(path);
+        }
+
+        if let Some(path) = &self.cached_binary_path {
+            if fs::metadata(path).map_or(false, |stat| stat.is_file()) {
+                return Ok(path.to_string());
+            }
+        }
+
+        zed::set_language_server_installation_status(
+            language_server_id,
+            &zed::LanguageServerInstallationStatus::CheckingForUpdate,
+        );
+
+        let release = zed::latest_github_release(
+            GITHUB_REPO,
+            zed::GithubReleaseOptions {
+                require_assets: true,
+                pre_release: true,
+            },
+        )?;
+
+        let (platform, arch) = zed::current_platform();
+
+        let arch: &str = match arch {
+            zed::Architecture::Aarch64 => "arm64",
+            zed::Architecture::X8664 => "x86_64",
+            zed::Architecture::X86 => return Err("Unsupported platform x86".into()),
+        };
+
+        let os: &str = match platform {
+            zed::Os::Mac => "darwin",
+            zed::Os::Linux => "unknown-linux-gnu",
+            zed::Os::Windows => "pc-windows-msvc",
+        };
+
+        let file_name = format!("ols-{arch}-{os}");
+        let asset_name = format!("{file_name}.zip");
+
+        let asset = release
+            .assets
+            .iter()
+            .find(|asset| asset.name == asset_name)
+            .ok_or_else(|| format!("no asset found matching {:?}", asset_name))?;
+
+        let version_dir = format!("ols-{}", release.version);
+        let binary_path = format!(
+            "{version_dir}/{file_name}{extension}",
+            extension = match platform {
+                zed::Os::Windows => ".exe",
+                _ => "",
+            },
+        );
+
+        if !fs::metadata(&binary_path).map_or(false, |stat| stat.is_file()) {
+            zed::set_language_server_installation_status(
+                language_server_id,
+                &zed::LanguageServerInstallationStatus::Downloading,
+            );
+
+            zed::download_file(
+                &asset.download_url,
+                &version_dir,
+                zed::DownloadedFileType::Zip,
+            )
+            .map_err(|e| format!("failed to download file: {e}"))?;
+
+            zed::make_file_executable(&binary_path)?;
+
+            // Cleanup older versions
+            let entries =
+                fs::read_dir(".").map_err(|e| format!("failed to list working directory {e}"))?;
+            for entry in entries {
+                let entry = entry.map_err(|e| format!("failed to load directory entry {e}"))?;
+                if entry.file_name().to_str() != Some(&version_dir) {
+                    fs::remove_dir_all(entry.path()).ok();
+                }
+            }
+        }
+
+        self.cached_binary_path = Some(binary_path.clone());
+        Ok(binary_path)
+    }
+}
+
+impl zed::Extension for OdinExtension {
+    fn new() -> Self {
+        Self {
+            cached_binary_path: None,
+        }
+    }
+
+    fn language_server_command(
+        &mut self,
+        language_server_id: &LanguageServerId,
+        worktree: &Worktree,
+    ) -> Result<zed::Command> {
+        let ols_binary_path = self.language_server_binary_path(language_server_id, worktree)?;
+        Ok(zed::Command {
+            command: ols_binary_path,
+            args: Default::default(),
+            env: Default::default(),
+        })
+    }
+
+    fn language_server_workspace_configuration(
+        &mut self,
+        language_server_id: &LanguageServerId,
+        worktree: &Worktree,
+    ) -> Result<Option<serde_json::Value>> {
+        let settings = LspSettings::for_worktree(language_server_id.as_ref(), worktree)
+            .ok()
+            .and_then(|lsp_settings| lsp_settings.settings.clone())
+            .unwrap_or_default();
+        Ok(Some(settings))
+    }
+}
+
+zed::register_extension!(OdinExtension);
