7e486bc42bffd41d91691e755edf20b8622e8bbf
Mo Nematzadeh <mohammad.h.nematzadeh@gmail.com>
2026-07-26 21:52:08 +0200

    feat: LLDB formatter improvements (#75)
    * feat: LLDB formatter improvements
    
    Expand debug section in readme
    
    * added bit_set summary to debug view

--- resources/lldb/odin.py
+++ resources/lldb/odin.py
@@ -1,9 +1,11 @@
 # Original author: laytan (https://gist.github.com/laytan/a94c323a84cef7bcfbdf6d21987fd5a9)
 # Modifications by: harold-b (https://gist.github.com/harold-b/ef16a5c3ebcceccfc2bc7a5c5dd0058d)
 # Modifications by: adneufeld
+# Modifications by: rxptr
 
 import logging
 import math
+import struct
 
 import lldb
 
@@ -29,6 +31,7 @@ def slice_summary(value, internal_dict):
 
 class SliceChildProvider:
     CHUNK_COUNT = 2000
+    MAX_ELEMENTS = 1 << 20
 
     def __init__(self, val, dict):
         self.val = val
@@ -37,9 +40,15 @@ class SliceChildProvider:
     def update(self):
         val = self.val
 
-        self.len = val.GetChildMemberWithName("len").unsigned
+        self.len = val.GetChildMemberWithName("len").GetValueAsSigned()
         self.data_val = val.GetChildMemberWithName("data")
-        assert self.data_val.type.is_pointer
+        if (
+            self.len < 0
+            or not self.data_val.type.is_pointer
+            or self.data_val.GetValueAsUnsigned() == 0
+        ):
+            self.len = 0
+        self.len = min(self.len, SliceChildProvider.MAX_ELEMENTS)
 
         is_chunked = self.len > SliceChildProvider.CHUNK_COUNT
         self.chunked_len = (
@@ -54,10 +63,12 @@ class SliceChildProvider:
         return self.chunked_len if self.chunked_len > 0 else self.len
 
     def get_child_at_index(self, index):
-        length = self.num_children()
-        assert index >= 0 and index < length
+        if index < 0 or index >= self.num_children():
+            return None
 
         first = self.data_val.deref
+        if not first.IsValid():
+            return None
 
         if self.chunked_len > 0:
             chunk_size = SliceChildProvider.CHUNK_COUNT
@@ -76,6 +87,9 @@ class SliceChildProvider:
         return self.data_val.CreateChildAtOffset(f"[{index}]", offset, first.type)
 
 
+MAX_DISPLAY_LENGTH = 4096
+
+
 def is_string_type(t, internal_dict):
     return t.name == "string"
 
@@ -83,30 +97,77 @@ def is_string_type(t, internal_dict):
 def string_summary(value, internal_dict):
     pointer = value.GetChildMemberWithName("data").GetValueAsUnsigned(0)
     length = value.GetChildMemberWithName("len").GetValueAsSigned(0)
-    if pointer == 0:
-        return False
-    if length == 0:
+    if length <= 0:
         return '""'
+    if pointer == 0:
+        return "nil"
     error = lldb.SBError()
-    string_data = value.process.ReadMemory(pointer, length, error)
-    return '"{}"'.format(string_data.decode("utf-8"))
+    read_length = min(length, MAX_DISPLAY_LENGTH)
+    string_data = value.process.ReadMemory(pointer, read_length, error)
+    if not error.success:
+        return "<unreadable>"
+    text = string_data.decode("utf-8", errors="replace")
+    suffix = "…" if length > read_length else ""
+    return '"{}{}"'.format(text, suffix)
+
+
 
 
 def is_map_type(t, internal_dict):
     return t.name.startswith("map[")
 
 
+def is_bit_set_type(t, internal_dict):
+    return t.name.startswith("bit_set[")
+
+
+def bit_set_summary(value, internal_dict):
+    if value.IsSynthetic():
+        value = value.GetNonSyntheticValue()
+
+    is_rune_range = "rune(" in value.type.name
+    members = []
+    for child in value.children:
+        name = child.name
+        if not name or child.type.name != "bool" or child.unsigned == 0:
+            continue
+        if name.isdigit():
+            codepoint = int(name)
+            members.append("'{}'".format(chr(codepoint)) if is_rune_range else name)
+        else:
+            members.append(".{}".format(name))
+    return "{{{}}}".format(", ".join(members))
+
+
+MAX_MAP_SCAN = 1 << 20
+TOMBSTONE_MASK = 1 << 63
+
+
+def map_summary(value, internal_dict):
+    value = value.GetNonSyntheticValue()
+    length = value.GetChildMemberWithName("len").GetValueAsSigned(0)
+    raw_data = value.GetChildMemberWithName("data").GetValueAsUnsigned(0)
+    cap_log2 = raw_data & 63
+    cap = 1 << cap_log2 if cap_log2 > 0 else 0
+    return "len = {}, cap = {}".format(length, cap)
+
+
 class MapChildProvider:
     def __init__(self, val, dict):
         self.val = val
+        self.update()
 
-    def num_children(self):
-        return (self.val.GetChildMemberWithName("len").GetValueAsSigned() * 2) + 1
+    def update(self):
+        self.entries = []
 
-    def get_child_at_index(self, index):
-        data = self.val.GetChildMemberWithName("data")
-        tkey = data.GetChildMemberWithName("key").type
-        tval = data.GetChildMemberWithName("value").type
+        val = self.val
+        length = val.GetChildMemberWithName("len").GetValueAsSigned()
+        data = val.GetChildMemberWithName("data")
+        if length <= 0 or not data.IsValid():
+            return False
+
+        self.tkey = data.GetChildMemberWithName("key").type
+        self.tval = data.GetChildMemberWithName("value").type
         hash_field = data.GetChildMemberWithName("hash")
         key_cell = data.GetChildMemberWithName("key_cell")
         value_cell = data.GetChildMemberWithName("value_cell")
@@ -114,56 +175,49 @@ class MapChildProvider:
         raw_data = data.GetValueAsUnsigned()
         key_ptr = raw_data & ~63
         cap_log2 = raw_data & 63
-        cap = 0 if cap_log2 <= 0 else 1 << cap_log2
+        cap = 1 << cap_log2 if cap_log2 > 0 else 0
+        if key_ptr == 0 or cap == 0 or hash_field.size != 8:
+            return False
 
-        key_cell_info = self.cell_info(tkey, key_cell)
-        value_cell_info = self.cell_info(tval, value_cell)
-
-        size_of_hash = hash_field.size
-        assert size_of_hash == 8
+        key_cell_info = self.cell_info(self.tkey, key_cell)
+        value_cell_info = self.cell_info(self.tval, value_cell)
 
         value_ptr = self.cell_index(key_ptr, key_cell_info, cap)
         hash_ptr = self.cell_index(value_ptr, value_cell_info, cap)
 
+        scan_cap = min(cap, MAX_MAP_SCAN)
         error = lldb.SBError()
+        hash_bytes = val.process.ReadMemory(hash_ptr, scan_cap * 8, error)
+        if not error.success:
+            return False
 
-        # Last one, the capacity.
-        if index == self.num_children() - 1:
-            cap_data = lldb.SBData.CreateDataFromInt(cap)
-            return self.val.CreateValueFromData(
-                "cap", cap_data, self.val.GetChildMemberWithName("len").type
-            )
-
-        wants_key = index % 2 == 0
-        index = int(index / 2)
-
-        key_index = 0
-        for i in range(cap):
-            TOMBSTONE_MASK = 1 << (size_of_hash * 8 - 1)
-
-            offset_hash = hash_ptr + i * size_of_hash
-
-            hash_val = self.val.process.ReadUnsignedFromMemory(
-                offset_hash, size_of_hash, error
-            )
-            if not error.success:
-                print(error)
+        for i, hash_val in enumerate(struct.unpack(f"<{scan_cap}Q", hash_bytes)):
+            if hash_val == 0 or (hash_val & TOMBSTONE_MASK) != 0:
                 continue
-            elif hash_val == 0 or (hash_val & TOMBSTONE_MASK) != 0:
-                continue
+            self.entries.append(
+                (
+                    self.cell_index(key_ptr, key_cell_info, i),
+                    self.cell_index(value_ptr, value_cell_info, i),
+                )
+            )
+            if len(self.entries) >= length:
+                break
 
-            offset_key = self.cell_index(key_ptr, key_cell_info, i)
-            offset_value = self.cell_index(value_ptr, value_cell_info, i)
+        return False
 
-            if index == key_index:
-                if wants_key:
-                    return self.val.CreateValueFromAddress(f"[{i}]", offset_key, tkey)
-                else:
-                    return self.val.CreateValueFromAddress(f"[{i}]", offset_value, tval)
+    def num_children(self):
+        return len(self.entries)
 
-            key_index += 1
+    def get_child_at_index(self, index):
+        if index < 0 or index >= len(self.entries):
+            return None
 
-        print("not found")
+        key_addr, value_addr = self.entries[index]
+        key = self.val.CreateValueFromAddress("key", key_addr, self.tkey)
+        label = key.GetSummary() or key.GetValue() or str(index)
+        return self.val.CreateValueFromAddress(
+            f"[{label}]", value_addr, self.tval
+        )
 
     def cell_info(self, typev, cell_type):
         elements_per_cell = 0
@@ -220,26 +274,23 @@ class UnionChildProvider:
 
     def update(self):
         self.children = self.val.children
-        self.variant_index = self.children[0].unsigned
+        self.variant_index = self.children[0].unsigned if self.children else 0
         return False
 
     def num_children(self):
-        return len(self.children) - 1
+        return max(0, len(self.children) - 1)
 
     def get_child_at_index(self, index):
+        if index < 0 or index >= self.num_children():
+            return None
+
         value = self.val
-
-        variant_index = index + 1
-        variant = self.children[variant_index]
+        variant = self.children[index + 1]
         name = variant.type.GetDisplayTypeName()
-        # offset        = variant.addr.offset - value.addr.offset
-        selected = "*" if self.variant_index == variant_index else ""
+        selected = "*" if variant.name == f"v{self.variant_index}" else ""
 
-        field_name = f"{selected}v{variant_index}({name})"
-        # c = value.CreateChildAtOffset( field_name, offset, variant.GetType() )
-        c = value.CreateValueFromData(field_name, variant.data, variant.type)
-
-        return c
+        field_name = f"{selected}{variant.name}({name})"
+        return value.CreateValueFromData(field_name, variant.data, variant.type)
 
 
 def is_type_union(t, internal_dict):
@@ -255,12 +306,12 @@ def union_summary(value, internal_dict):
         value = value.GetNonSyntheticValue()
 
     tag = value.GetChildAtIndex(0)
-    assert tag.name == "tag"
-    # tag = value.GetChildMemberWithName("tag")
+    if not tag.IsValid() or tag.name != "tag":
+        return ""
 
-    variant_name = f"v{tag.unsigned}"
-    variant = value.GetChildMemberWithName(variant_name)
-    # variant_type = variant.type.GetDisplayTypeName()
+    variant = value.GetChildMemberWithName(f"v{tag.unsigned}")
+    if not variant.IsValid():
+        return "nil"
 
     return f"{variant}"
 
@@ -285,4 +336,10 @@ def __lldb_init_module(debugger, unused):
 
     debugger.HandleCommand(
         "type synth add --recognizer-function --python-class odin.MapChildProvider odin.is_map_type"
+    )
+    debugger.HandleCommand(
+        "type summary add --recognizer-function --python-function odin.map_summary odin.is_map_type"
+    )
+    debugger.HandleCommand(
+        "type summary add --recognizer-function --python-function odin.bit_set_summary odin.is_bit_set_type"
     )
